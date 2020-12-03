acb12862e8397c157abcd2b1fc0066e09a7a3d2e
Dylano Brouwer <99064035@mydavinci.nl>
2020-12-03 21:34:31 +0100

    commit 2

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-var direction = ['N, NE, E, SE, S, SW, W, NW'];
-var randomItem = direction[Math.floor(Math.random() * direction.length)];
-var duck = document.getElementById('duck');
-duck.addEventListener("click", function() {
-	duck.innerHTML = alert("hello world");
-})
+var dir = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+var duck = document.getElementById("duck");
+var posTop = "150px";
+var posLeft = "550";
+var posRight = "0";
+var posBottom = "0";
+
+function duckClick() {
+	document.getElementById("duck").src = "images/explosion.gif";
+	setTimeout(function() {
+		document.getElementById("duck").src = "images/duck.gif";
+	}, 800);	
+}
+
+function fly(direction) {
+	var number = Math.floor(Math.random() * dir.length);
+	direction = dir;
+	console.log(dir[number]);
+	if(dir[number] == 'N') {
+		duck.style.marginTop = posTop + "75px";
+	}
+}
+
+function moveDuck() {
+	setInterval(function() {
+		fly();
+	},500);
+}
+moveDuck();
